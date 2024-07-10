be5c3fa319b75fbb0ba90dc1ad8e5a2450cc51c6
Islam Ibakaev <islamibakaev@gmail.com>
2024-07-11 02:14:06 +0300

    display chapter and lesson titles

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>chapter {{ chapter.title}}</h2>
+  <h3>lesson {{ lesson.title }}</h3>
+  <p>This is a lesson text</p>
+</template>
+
+<script setup>
+const course = useCourse()
+const route = useRoute()
+console.log(course)
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    chapter => chapter.slug === route.params.chapterSlug
+  )
+})
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    lesson => lesson.slug === route.params.lessonSlug
+  )
+})
+</script>
--- pages/course/lesson.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h2>lesson</h2>
-  <p>This is a lesson text</p>
-</template>
