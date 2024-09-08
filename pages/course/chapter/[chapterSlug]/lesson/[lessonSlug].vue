9e8dbd095eb27c960f3a2fc6c0689d8a8b265916
Islam Ibakaev <islamibakaev@gmail.com>
2024-09-08 03:55:46 +0300

    add button to for course progress tracking

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -5,28 +5,20 @@
 		</p>
 		<h2 class="my-0">{{ lesson.title }}</h2>
 		<div class="flex mt-2 mb-8 space-x-4">
-			<NuxtLink
-				v-if="lesson.sourceUrl"
-				class="font-normal text-gray-500 text-md"
-				:to="lesson.sourceUrl"
-			>
+			<NuxtLink v-if="lesson.sourceUrl"
+				class="font-normal text-gray-500 text-md" :to="lesson.sourceUrl">
 				Download Source Code
 			</NuxtLink>
-			<NuxtLink
-				v-if="lesson.downloadUrl"
-				class="font-normal text-gray-500 text-md"
-				:to="lesson.downloadUrl"
-			>
+			<NuxtLink v-if="lesson.downloadUrl"
+				class="font-normal text-gray-500 text-md" :to="lesson.downloadUrl">
 				Download Video
 			</NuxtLink>
 		</div>
 		<VideoPlayer v-if="lesson.videoId" :video-id="lesson.videoId" />
 		<p>{{ lesson.text }}</p>
 
-		<!-- <LessonCompleteButton
-			:model-value="isLessonComplete"
-			@update:model-value="toggleComplete"
-		/> -->
+		<LessonCompleteButton :model-value="isLessonComplete"
+			@update:model-value="toggleComplete" />
 	</div>
 </template>
 
@@ -50,30 +42,30 @@ const title = computed(() => {
 	return `${lesson.value.title} - ${course.title}`;
 });
 
-// useHead({
-// 	title,
-// });
+useHead({
+	title,
+});
 
-// const progress = useLocalStorage('progress', []);
+const progress = useLocalStorage('progress', []);
 
-// const isLessonComplete = computed(() => {
-// 	if (!progress.value[chapter.value.number - 1]) {
-// 		return false;
-// 	}
+const isLessonComplete = computed(() => {
+	if (!progress.value[chapter.value.number - 1]) {
+		return false;
+	}
 
-// 	if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
-// 		return false;
-// 	}
+	if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+		return false;
+	}
 
-// 	return progress.value[chapter.value.number - 1][lesson.value.number - 1];
-// });
+	return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+});
 
-// const toggleComplete = () => {
-// 	if (!progress.value[chapter.value.number - 1]) {
-// 		progress.value[chapter.value.number - 1] = [];
-// 	}
+const toggleComplete = () => {
+	if (!progress.value[chapter.value.number - 1]) {
+		progress.value[chapter.value.number - 1] = [];
+	}
 
-// 	progress.value[chapter.value.number - 1][lesson.value.number - 1] =
-// 		!isLessonComplete.value;
-// };
+	progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+		!isLessonComplete.value;
+};
 </script>
