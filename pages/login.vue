56a996612080ad59366d99eb15a48246721172a9
Islam Ibakaev <islamibakaev@gmail.com>
2025-01-01 04:41:14 +0300

    github login added

--- pages/login.vue
+++ pages/login.vue
@@ -3,6 +3,7 @@
     <h1>Log in to {{ title }}</h1>
     <button
       class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      @click="login"
     >
       Log in with Github
     </button>
@@ -13,7 +14,7 @@
 const { title } = useCourse()
 // const course = await useCourse();
 // const { query } = useRoute();
-// const supabase = useSupabaseClient();
+const supabase = useSupabaseClient();
 // const user = useSupabaseUser();
 
 // watchEffect(async () => {
@@ -24,15 +25,15 @@ const { title } = useCourse()
 //   }
 // });
 
-// const login = async () => {
-//   const redirectTo = `${window.location.origin}${query.redirectTo}`;
-//   const { error } = await supabase.auth.signInWithOAuth({
-//     provider: 'github',
-//     options: { redirectTo },
-//   });
+const login = async () => {
+  // const redirectTo = `${window.location.origin}${query.redirectTo}`;
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    // options: { redirectTo },
+  });
 
-//   if (error) {
-//     console.error(error);
-//   }
-// };
+  if (error) {
+    console.error(error);
+  }
+};
 </script>
